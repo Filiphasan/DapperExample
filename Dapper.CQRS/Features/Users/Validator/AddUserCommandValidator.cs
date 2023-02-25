bcce76fee56a9fcdd44fa0aa31e4dd13ea985516
Hasan Erdal <hasaerda@hotmail.com>
2023-02-25 13:39:57 +0300

    edit adduser validator

--- Dapper.CQRS/Features/Users/Validator/AddUserCommandValidator.cs
+++ Dapper.CQRS/Features/Users/Validator/AddUserCommandValidator.cs
@@ -7,9 +7,9 @@ public class AddUserCommandValidator : AbstractValidator<AddUserCommand>
 {
     public AddUserCommandValidator()
     {
-        RuleFor(x => x.FirstName).NotNull().NotEmpty().WithMessage("First Name is required.");
-        RuleFor(x => x.LastName).NotNull().NotEmpty().WithMessage("Last Name is required.");
-        RuleFor(x => x.UserName).NotNull().NotEmpty().WithMessage("Username is required.");
-        RuleFor(x => x.Password).NotNull().NotEmpty().WithMessage("Password is required.");
+        RuleFor(x => x.FirstName).NotNull().WithMessage("First Name is required.").NotEmpty().WithMessage("First Name is required.");
+        RuleFor(x => x.LastName).NotNull().WithMessage("Last Name is required.").NotEmpty().WithMessage("Last Name is required.");
+        RuleFor(x => x.UserName).NotNull().WithMessage("Username is required.").NotEmpty().WithMessage("Username is required.");
+        RuleFor(x => x.Password).NotNull().WithMessage("Password is required.").NotEmpty().WithMessage("Password is required.");
     }
 }
